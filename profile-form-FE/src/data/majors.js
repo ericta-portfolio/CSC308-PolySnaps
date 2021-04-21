0a9fd13b8e0c83afcafb8ee6793e2e98aa331074
ericta-portfolio <ericcta@gmail.com>
2021-04-21 14:53:24 -0700

    Added compareProfile algorithm and Gender+Major

---
+++ profile-form-FE/src/data/majors.js
@@ -0,0 +1,10 @@
+const majorsList = [
+  "Agriculture, Food, and Environmental Sciences",
+  "Architecture and Environmental Design",
+  "Business",
+  "Engineering",
+  "Liberal Arts",
+  "Science and Mathematics"
+];
+
+export default majorsList;
--- profile-form-FE/src/index.js
+++ profile-form-FE/src/index.js
@@ -1,10 +1,12 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <div>
+  <StrictMode>
     <App />
-  </div>,
+  </StrictMode>,
   rootElement
 );
