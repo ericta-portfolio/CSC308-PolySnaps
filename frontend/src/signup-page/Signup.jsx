b0bee529f2148a2de33b357dc3d3d535d49e539d
Jake Noe <67278790+jpnoe@users.noreply.github.com>
2021-05-09 20:16:13 -0700

    fixing date bug

--- frontend/src/signup-page/Signup.jsx
+++ frontend/src/signup-page/Signup.jsx
@@ -8,7 +8,10 @@ function Signup() {
   const signup = (details) => {
     console.log(details);
 
-    if (details.password === details.password2 && details.date !== null) {
+    if (details.password === details.password2) {
+      if (details.date == null) {
+        details.date = new Date();
+      } 
       axios
         .post("http://localhost:5000/newUser", details)
         .then(function (response) {
