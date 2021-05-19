67275aab5e50c538909010b520bb881e1ac492ce
Jake Noe <67278790+jpnoe@users.noreply.github.com>
2021-05-19 12:03:20 -0700

    Revert "syncing heroku with main"
    This reverts commit d019129dc557c4ebc1c1c9af26a41a94fa8e61cf.

--- src/profile-form-FE/ProfileFormPage.jsx
+++ src/profile-form-FE/ProfileFormPage.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactForm from "./ReactForm";
-import ImgUpload from "./ImgUpload";
 
 function ProfileFormPage() {
   return (
     <div>
-      <h1>Upload Your Profile Picture:</h1>
-      <ImgUpload />
       <h1> Profile Form: </h1>
       <ReactForm />
     </div>
