ecd19b66cad9d777659d567cc4150f19ca292506
Jake Noe <67278790+jpnoe@users.noreply.github.com>
2021-05-30 16:15:42 -0700

    formatting update

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify, request, url_for
 from flask_cors import CORS
-import ssl
-import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
 from compareProfiles import compareProfiles
 
@@ -13,24 +11,21 @@ app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb+srv://polysnaps:cpw2021@cluster0.2oaoq.mongodb.net/mydatabase?retryWrites=true&w=majority"
 app.config['CORS_HEADERS'] = 'Content-Type'
-#CORS stands for Cross Origin Requests.
-CORS(app) #Here we'll allow requests coming from any domain.
-# Not recommended for production environment.
+CORS(app)
 
-mongo = PyMongo(app) #initializing the app variable
+mongo = PyMongo(app)
 
-## Configuring collection name we are going to work with
-## db_operations = mongo.db.<COLLECTION_NAME>
 db_operations = mongo.db.newUsers
 db_operations2 = mongo.db.profiles
 db_operations3 = mongo.db.accepted
 db_operations4 = mongo.db.rejected
 
-#All the routings in our app will be mentioned here.
+
 @app.route('/test')
 def test():
-    db_operations.delete_many({"first":"Test"})
+    db_operations.delete_many({"first": "Test"})
     return "App is working perfectly"
+
 
 @app.route('/profileUser/<id>', methods=['PUT'])
 def add_info_users(id):
@@ -45,28 +40,29 @@ def add_info_users(id):
         _partying = _json['partying']
         _major = _json['major']
         _id = ObjectId(_id)
-    except :
+    except:
         return not_found()
     db_operations.update_one({
         '_id': _id
-        },
-            {
-                '$set' : {
-                    'personality': _personality,
-                    'romance': _romance,
-                    'friendship': _friendship,
-                    'hobbies': _hobbies,
-                    'spirituality': _spirituality,
-                    'partying': _partying,
-                    'major': _major
-                }
-            }
-        )
+    },
+        {
+        '$set': {
+            'personality': _personality,
+            'romance': _romance,
+            'friendship': _friendship,
+            'hobbies': _hobbies,
+            'spirituality': _spirituality,
+            'partying': _partying,
+            'major': _major
+        }
+    }
+    )
     user = db_operations.find_one({
         '_id': ObjectId(_id)
     })
     user = stringify_userid(user)
     return user["_id"], 201
+
 
 @app.route('/newUser', methods=['POST'])
 def add_users():
@@ -80,11 +76,10 @@ def add_users():
         _date = _json['date']
     except:
         return "field error", 400
-        
+
     if _email == "" or _password == "" or _gender == "" or _first == "" or _last == "":
         return "Please fill out the entire form!", 400
     _hashed_password = generate_password_hash(_password)
-    # check if the email already exists
     all_users = list(db_operations.find())
     email_list = get_user_email(all_users)
     if _email in email_list:
@@ -104,7 +99,8 @@ def add_users():
     })
     user = stringify_userid(user)
     return user["_id"], 201
-    
+
+
 @app.route('/acceptMatch/<id>', methods=['POST'])
 def accept_match(id):
     _json = request.get_json()
@@ -122,6 +118,7 @@ def accept_match(id):
     db_operations3.insert({"id": id, "matches": matches})
     return "success", 201
 
+
 @app.route('/getAccepted/<id>', methods=['GET'])
 def get_accepted(id):
     try:
@@ -136,14 +133,16 @@ def get_accepted(id):
         except:
             return "no accepted matches", 200
         matches = list(map(str, matches))
-        matches = list(map(lambda x: db_operations.find_one({'_id': ObjectId(x)}), matches))
+        matches = list(map(lambda x: db_operations.find_one(
+            {'_id': ObjectId(x)}), matches))
         matches = list(filter(lambda x: x is not None, matches))
         matches = list(map(stringify_userid, matches))
         matches = list(map(lambda x: get_scores(x, userProf), matches))
         matches = sorted(matches, key=lambda k: k["score"], reverse=True)
         return dumps(matches), 200
     return "no accepted matches", 200
-    
+
+
 @app.route('/rejectMatch/<id>', methods=['POST'])
 def reject_match(id):
     _json = request.get_json()
@@ -161,6 +160,7 @@ def reject_match(id):
     db_operations4.insert({"id": id, "matches": matches})
     return "success", 201
 
+
 @app.route('/retrieve_all', methods=['GET'])
 def get_users():
     if request.method == 'GET':
@@ -168,13 +168,14 @@ def get_users():
         users = list(map(stringify_userid, users))
         return dumps(users), 200
 
+
 @app.route('/matches', methods=['POST'])
 def get_matches():
     _json = request.get_json()
     try:
         _id = ObjectId(_json['id'])
     except:
-        resp = jsonify({"message":"id error"})
+        resp = jsonify({"message": "id error"})
         resp.status_code = 400
         return resp
     users = list(db_operations.find())
@@ -191,7 +192,8 @@ def get_matches():
         if "romance" in user.keys():
             users = list(filter(lambda x: "romance" in x.keys(), users))
             users = list(map(lambda x: get_scores(x, user), users))
-            users = list(filter(lambda x: x["score"] > 0 and str(x["_id"]) != str(_id), users))
+            users = list(filter(lambda x: x["score"] > 0 and str(
+                x["_id"]) != str(_id), users))
             users = sorted(users, key=lambda k: k["score"], reverse=True)
             if rejected:
                 try:
@@ -203,6 +205,7 @@ def get_matches():
             return dumps(users), 200
     return not_found()
 
+
 @app.route('/users', methods=['POST'])
 def check_user():
     _json = request.get_json()
@@ -210,11 +213,11 @@ def check_user():
         _email = _json['email']
         _password = _json['password']
     except:
-        resp = jsonify({"message":"Email/Password error"})
+        resp = jsonify({"message": "Email/Password error"})
         resp.status_code = 400
         return resp
     get_info = db_operations.find_one({
-            'email': _email
+        'email': _email
     })
     if _email == "" and _password == "":
         return "Email and Password left blank!", 400
@@ -224,6 +227,7 @@ def check_user():
             user = stringify_userid(get_info)
             return user["_id"], 200
     return "Incorrect username or password", 404
+
 
 @app.route('/getUser/<id>', methods=['GET'])
 def get_user(id):
@@ -240,16 +244,15 @@ def get_user(id):
         return user, 200
     return not_found()
 
+
 @app.route('/profile_pic_upload/<id>', methods=['PUT'])
 def upload(id):
     if 'image' in request.files:
-        #create a file object
         image = request.files['image']
         try:
             id = ObjectId(id)
         except:
             return "id error", 400
-        #save_file params (file_name, "actual file data (binary data)")
         user = db_operations.find_one({
             '_id': id
         })
@@ -257,38 +260,40 @@ def upload(id):
             mongo.save_file(image.filename, image)
             db_operations.update_one({
                 '_id': id
-                    },
-                    {
-                        '$set' : {
-                            'image' : image.filename
-                        }
-                        
-                    })
+            },
+                {
+                '$set': {
+                    'image': image.filename
+                }
+
+            })
             resp = jsonify("picture added and user updated successfully!")
             resp.status_code = 201
             return resp
     return not_found()
 
-#send file
+
 @app.route('/file/<filename>')
 def file(filename):
     return mongo.send_file(filename)
+
 
 @app.route('/profile_pic_retrieve/<id>', methods=['GET'])
 def profile(id):
     if request.method == 'GET':
         _id = id
         user = db_operations.find_one({
-                '_id': ObjectId(_id)
+            '_id': ObjectId(_id)
         })
         if user:
-            filename= user['image']
+            filename = user['image']
             return ("https://polysnaps-be.herokuapp.com/file/" + str(filename))
     return not_found()
 
+
 @app.errorhandler(404)
 def not_found(error=None):
-    message={
+    message = {
         'status': 404,
         'message': 'Error! Try Again! ' + request.url
     }
@@ -296,18 +301,22 @@ def not_found(error=None):
     resp.status_code = 404
     return resp
 
+
 def stringify_userid(user):
     user["_id"] = str(user["_id"])
     user["password"] = None
     return user
 
+
 def get_scores(user, match):
     user["score"] = compareProfiles(user, match)
     return user
 
+
 def get_user_email(user):
     email_list = [e["email"] for e in user]
     return email_list
+
 
 if __name__ == "__main__":
     app.run(debug=True)
