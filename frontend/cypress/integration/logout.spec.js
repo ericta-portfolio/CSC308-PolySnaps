fdcec70477e777417fc87bbf2218b402dc8add93
Leticia <lalvessi@calpoly.edu>
2021-05-30 02:08:35 -0300

    logout cypress testing

---
+++ frontend/cypress/integration/logout.spec.js
@@ -0,0 +1,22 @@
+describe("/Login", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/Login");
+    });
+  
+    it("greets with Sign in", () => {
+      cy.contains("h1", "Sign In");
+    });
+  
+    it("Successful Log In", () => {
+        cy.get("[name=email]").type("lalvessi@calpoly.edu");
+        cy.get("[name=password]").type("123{enter}");
+        cy.location("pathname").should("match", /\/ProfileForm$/);
+      });
+
+    it("log out successfully", () => {
+        cy.get('[id=log]').contains('Log Out').click()
+        cy.location("pathname").should("match", /\/$/);
+    });
+    
+  });
+  
