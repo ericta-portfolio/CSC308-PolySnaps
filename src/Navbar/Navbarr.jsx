b36a6126f03c39ce00a31ce67960a05bf946a1c2
Jake Noe <67278790+jpnoe@users.noreply.github.com>
2021-05-31 12:03:20 -0700

    fixing cypress

--- src/Navbar/Navbarr.jsx
+++ src/Navbar/Navbarr.jsx
@@ -76,7 +76,7 @@ export default class Navbarr extends Component {
                 <Nav.Link href="/AcceptedPage" id="link2">
                   Accepted Matches
                 </Nav.Link>
-                <Button variant="secondary" onClick={this.state.funct} id="log">
+                <Button variant="secondary" name="logbutton" onClick={this.state.funct} id="log">
                   {this.state.text}
                 </Button>
               </Nav>
