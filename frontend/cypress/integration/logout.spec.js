785e816d557c5a33a5ed2c6f7641f1c8c91dafd0
Leticia <lalvessi@calpoly.edu>
2021-05-31 18:01:26 -0300

    given when cypress

--- frontend/cypress/integration/logout.spec.js
+++ frontend/cypress/integration/logout.spec.js
@@ -2,18 +2,10 @@ describe("/Logout", () => {
     beforeEach(() => {
       cy.visit("http://localhost:3000/Login");
     });
-
-    it("greets with Sign in", () => {
-      cy.contains("h1", "Sign In");
-    });
-
-    it("Successful Log In", () => {
-        cy.get("[name=email]").type("lalvessi@calpoly.edu");
-        cy.get("[name=password]").type("123");
-        cy.get("[type=submit]").contains("Log In").click();
-        cy.location("pathname").should("match", /\/ProfileForm$/);
-      });
-
+    
+//Given: I successfully log in
+//When: I press the log out button
+//Then: I get redirected to the Homepage and I'm logged out.
     it("log out successfully", () => {
         cy.get("[name=email]").type("lalvessi@calpoly.edu");
         cy.get("[name=password]").type("123");
--- frontend/cypress/integration/signup.spec.js
+++ frontend/cypress/integration/signup.spec.js
@@ -13,11 +13,18 @@ describe("/Signup", () => {
     cy.visit("http://localhost:3000/Signup");
   });
 
+  
+//Given: I fill out part of the Sign Up form
+//When: I click the Sign Up button
+//Then: the page displays "Please fill out the entire form!"
   it("Form not complete", () => {
     cy.get("form").contains("Sign Up").click();
     cy.get("[id=failed]").contains("Please fill out the entire form!");
   });
 
+//Given: I fill out the entire Sign Up form and input an existing email
+//When: I click the Sign Up button
+//Then: the page displays "Account already exists! Please sign-in :)"
   it("using an existing email", () => {
     cy.get("[name=first]").type("Leticia");
     cy.get("[name=last]").type("Siqueira");
@@ -29,6 +36,9 @@ describe("/Signup", () => {
     cy.get("[id=failed]").contains("Account already exists! Please sign-in :)");
   });
 
+//Given: I fill out the entire Sign Up form and input mismatching passwords
+//When: I click the Sign Up button
+//Then: the page displays "Passwords do not match!"
   it("Password confirmation rejected", () => {
     cy.get("[name=first]").type("JayJay");
     cy.get("[name=last]").type("the Jet Plane");
@@ -40,8 +50,9 @@ describe("/Signup", () => {
     cy.get("[id=failed]").contains("Passwords do not match!");
   });
 
-  //check for emails that aren't calpoly
-
+//Given: I fill out the entire Sign Up form and input a nonexistent email and matching passwords
+//When: I click the Sign Up button
+//Then: I successfully create an account and it redirects me to the Profile Form page
   it("Successful Sign up", () => {
     getfn = getElement(othernames);
     getlastname = getElement(lastname);
