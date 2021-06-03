a3f81093c07439b3441af83690b01921d2b9617a
Jake Noe <67278790+jpnoe@users.noreply.github.com>
2021-06-03 10:54:41 -0700

    prettier

--- src/profile-form-FE/ReactForm.jsx
+++ src/profile-form-FE/ReactForm.jsx
@@ -270,7 +270,8 @@ export default function ReactForm() {
                 </label>
               </div>
             );
-          }})}
+          }
+        })}
         {/* Friendship */}
         <h1 className="titles">Friendship Interest:</h1>
         {interestsList.map((friend) => {
@@ -308,7 +309,8 @@ export default function ReactForm() {
                 </label>
               </div>
             );
-        }})}
+          }
+        })}
         {/* Hobbies */}
         <h1 className="titles">Hobbies:</h1>
         {hobbiesList.map((hobby) => (
