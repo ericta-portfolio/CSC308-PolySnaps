f475427fa8284cd7db3b37679c21d86dfba3b734
Leticia Siqueira <47129446+lalvessiqueira@users.noreply.github.com>
2021-06-03 14:12:36 -0300

    rounding score

--- src/AcceptedPage/Card.jsx
+++ src/AcceptedPage/Card.jsx
@@ -81,7 +81,7 @@ function Card(props) {
           <Detail label="Hobbies" detailInfo={hobbies} />
           <Detail label="Friendship" detailInfo={friendship} />
           <Detail label="Partying" detailInfo={partying} />
-          <Detail label="Score" detailInfo={props.score} />
+          <Detail label="Score" detailInfo={Math.round(props.score)} />
         </dd>
       </div>
     </div>
